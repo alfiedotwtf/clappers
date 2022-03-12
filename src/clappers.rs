ea731717fda40e60b958ec7277f8d09b930da800
Alfie John <alfie@alfie.wtf>
2022-03-12 17:36:35 +1100

    Cleaning up code

--- src/clappers.rs
+++ src/clappers.rs
@@ -2,138 +2,34 @@ use std::collections::HashMap;
 use std::collections::HashSet;
 use std::env;
 
-#[derive(Clone, Debug)]
-pub struct Clappers {
-    config: ClappersConfig,
-    values: ClappersValues,
-}
-
-#[derive(Clone, Debug)]
-pub struct ClappersConfig {
-    flags: HashSet<String>,
-    flag_aliases: HashMap<String, String>,
-    single: HashSet<String>,
-    single_aliases: HashMap<String, String>,
-    multiple: HashSet<String>,
-    multiple_aliases: HashMap<String, String>,
-}
-
-#[derive(Clone, Debug)]
-pub struct ClappersValues {
-    flags: HashSet<String>,
-    single: HashMap<String, String>,
-    multiple: HashMap<String, Vec<String>>,
-}
-
 impl Clappers {
     pub fn build() -> Self {
         Self {
-            config: ClappersConfig {
-                flags: HashSet::new(),
-                flag_aliases: HashMap::new(),
-                single: HashSet::new(),
-                single_aliases: HashMap::new(),
-                multiple: HashSet::new(),
-                multiple_aliases: HashMap::new(),
+            config: Config {
+                flags: ConfigType::new(),
+                singles: ConfigType::new(),
+                multiples: ConfigType::new(),
             },
-            values: ClappersValues {
+            values: Values {
                 flags: HashSet::new(),
-                single: HashMap::new(),
-                multiple: HashMap::new(),
+                singles: HashMap::new(),
+                multiples: HashMap::new(),
             },
-        }
-    }
-
-    pub fn get_flag(&self, parameter: &str) -> bool {
-        match self.config.flag_aliases.get(parameter) {
-            None => false,
-            Some(real_flag) => self.values.flags.contains(real_flag),
-        }
-    }
-
-    pub fn get_single(&self, parameter: &str) -> String {
-        match self.config.single_aliases.get(parameter) {
-            None => "".to_string(),
-            Some(real_single) => self
-                .values
-                .single
-                .get(real_single)
-                .unwrap_or(&"".to_string())
-                .to_string(),
-        }
-    }
-
-    pub fn get_multiple(&self, parameter: &str) -> Vec<String> {
-        match self.config.multiple_aliases.get(parameter) {
-            None => {
-                vec![]
-            }
-            Some(real_multiple) => self
-                .values
-                .multiple
-                .get(real_multiple)
-                .unwrap_or(&vec![])
-                .to_vec(),
         }
     }
 
     pub fn flags(mut self, argument_specs: Vec<&str>) -> Self {
-        for argument_spec in argument_specs {
-            let arguments: Vec<&str> = argument_spec.split('|').collect();
-
-            if arguments.is_empty() {
-                continue;
-            }
-
-            self.config.flags.insert(arguments[0].to_string());
-
-            for argument in &arguments {
-                self.config
-                    .flag_aliases
-                    .insert(argument.to_string(), arguments[0].to_string());
-            }
-        }
-
-        self
-    }
-
-    pub fn multiples(mut self, argument_specs: Vec<&str>) -> Self {
-        for argument_spec in argument_specs {
-            let arguments: Vec<&str> = argument_spec.split('|').collect();
-
-            if arguments.is_empty() {
-                continue;
-            }
-
-            self.config.multiple.insert(arguments[0].to_string());
-
-            for argument in &arguments {
-                self.config
-                    .multiple_aliases
-                    .insert(argument.to_string(), arguments[0].to_string());
-            }
-        }
-
+        self.config.flags.add_to_config(argument_specs);
         self
     }
 
     pub fn singles(mut self, argument_specs: Vec<&str>) -> Self {
-        for argument_spec in argument_specs {
-            let arguments: Vec<&str> = argument_spec.split('|').collect();
+        self.config.singles.add_to_config(argument_specs);
+        self
+    }
 
-            if arguments.is_empty() {
-                continue;
-            }
-
-            self.config.single.insert(arguments[0].to_string());
-
-            for argument in &arguments {
-                self.config
-                    .single_aliases
-                    .insert(argument.to_string(), arguments[0].to_string());
-            }
-        }
-
+    pub fn multiples(mut self, argument_specs: Vec<&str>) -> Self {
+        self.config.multiples.add_to_config(argument_specs);
         self
     }
 
@@ -151,21 +47,21 @@ impl Clappers {
                     next = next.split_off(1);
                 }
 
-                if let Some(real_flag) = self.config.flag_aliases.get(&next) {
-                    self.values.flags.insert(real_flag.to_string());
-                } else if let Some(real_single) = self.config.single_aliases.get(&next) {
+                if let Some(name) = self.config.flags.aliases.get(&next) {
+                    self.values.flags.insert(name.to_string());
+                } else if let Some(name) = self.config.singles.aliases.get(&next) {
                     if let Some(v) = args.peek() {
                         if v.starts_with('-') {
                             continue;
                         } else {
                             self.values
-                                .single
-                                .insert(real_single.to_string(), args.next().unwrap());
+                                .singles
+                                .insert(name.to_string(), args.next().unwrap());
                         }
                     }
-                } else if let Some(real_multiple) = self.config.multiple_aliases.get(&next) {
-                    if self.values.multiple.get_mut(real_multiple).is_none() {
-                        self.values.multiple.insert(real_multiple.clone(), vec![]);
+                } else if let Some(name) = self.config.multiples.aliases.get(&next) {
+                    if self.values.multiples.get_mut(name).is_none() {
+                        self.values.multiples.insert(name.clone(), vec![]);
                     }
 
                     while let Some(value) = args.peek() {
@@ -173,22 +69,110 @@ impl Clappers {
                             break;
                         } else {
                             self.values
-                                .multiple
-                                .get_mut(real_multiple)
+                                .multiples
+                                .get_mut(name)
                                 .unwrap()
                                 .push(args.next().unwrap());
                         }
                     }
                 }
             } else {
-                if self.values.multiple.get_mut("").is_none() {
-                    self.values.multiple.insert("".to_string(), vec![]);
+                if self.values.multiples.get_mut("").is_none() {
+                    self.values.multiples.insert("".to_string(), vec![]);
                 }
 
-                self.values.multiple.get_mut("").unwrap().push(next);
+                self.values.multiples.get_mut("").unwrap().push(next);
             }
         }
 
         self
     }
+
+    pub fn get_flag(&self, parameter: &str) -> bool {
+        self.config
+            .flags
+            .aliases
+            .get(parameter)
+            .map_or(false, |f| self.values.flags.contains(f))
+    }
+
+    pub fn get_single(&self, parameter: &str) -> String {
+        self.config
+            .singles
+            .aliases
+            .get(parameter)
+            .map_or("".to_string(), |s| {
+                self.values
+                    .singles
+                    .get(s)
+                    .unwrap_or(&"".to_string())
+                    .to_string()
+            })
+    }
+
+    pub fn get_multiple(&self, parameter: &str) -> Vec<String> {
+        self.config
+            .multiples
+            .aliases
+            .get(parameter)
+            .map_or(vec![], |m| {
+                self.values.multiples.get(m).unwrap_or(&vec![]).to_vec()
+            })
+    }
+
+    pub fn get_leftovers(&self) -> Vec<String> {
+        self.get_multiple("")
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Clappers {
+    config: Config,
+    values: Values,
+}
+
+#[derive(Clone, Debug)]
+struct Config {
+    flags: ConfigType,
+    singles: ConfigType,
+    multiples: ConfigType,
+}
+
+#[derive(Clone, Debug)]
+struct Values {
+    flags: HashSet<String>,
+    singles: HashMap<String, String>,
+    multiples: HashMap<String, Vec<String>>,
+}
+
+impl ConfigType {
+    fn new() -> Self {
+        Self {
+            name: HashSet::new(),
+            aliases: HashMap::new(),
+        }
+    }
+
+    fn add_to_config(&mut self, argument_specs: Vec<&str>) {
+        for argument_spec in argument_specs {
+            let arguments: Vec<&str> = argument_spec.split('|').collect();
+
+            if arguments.is_empty() {
+                continue;
+            }
+
+            self.name.insert(arguments[0].to_string());
+
+            for argument in &arguments {
+                self.aliases
+                    .insert(argument.to_string(), arguments[0].to_string());
+            }
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+struct ConfigType {
+    name: HashSet<String>,
+    aliases: HashMap<String, String>,
 }
